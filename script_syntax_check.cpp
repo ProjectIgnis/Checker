0a8f06689e75de72f92b2e994701607bde2e15ab
kevinlul <6320810+kevinlul@users.noreply.github.com>
2019-11-01 19:59:10 -0400

    Rewrite syntax check to use Linux dirent.h instead of C++17 stdfs

--- script_syntax_check.cpp
+++ script_syntax_check.cpp
@@ -15,14 +15,17 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include <algorithm>
+
 #include <cstring>
-#include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <vector>
+#include <sstream>
+
+#include <dirent.h>
+
 #include "ocgapi.h"
 #include "common.h"
+
 
 int GetCard([[maybe_unused]] void *payload, int code, OCG_CardData *card) {
     memset(card, 0, sizeof(OCG_CardData));
@@ -34,7 +37,7 @@ int LoadScript([[maybe_unused]] void *payload, OCG_Duel duel, const char *path) 
     std::ifstream f(path);
     std::stringstream buf;
     buf << f.rdbuf();
-    return buf.str().length() && OCG_LoadScript(duel, buf.str().c_str(), buf.str().length(), std::filesystem::path(path).filename().c_str());
+    return buf.str().length() && OCG_LoadScript(duel, buf.str().c_str(), buf.str().length(), path);
 }
 
 int exitCode = EXIT_SUCCESS;
@@ -44,15 +47,10 @@ void Log([[maybe_unused]] void *payload, const char *string, int type) {
 }
 
 void LoadIfExists(OCG_Duel duel, const char* script) {
-    std::error_code error;
-    if (std::filesystem::is_regular_file(script, error)) {
-        if (LoadScript(nullptr, duel, script)) {
-            std::cout << "Loaded " << script << std::endl;
-        } else {
-            std::cerr << "Failed to load " << script << std::endl;
-        }
+    if (LoadScript(nullptr, duel, script)) {
+        std::cout << "Loaded " << script << std::endl;
     } else {
-        std::cout << script << " not found" << std::endl;
+        std::cerr << "Failed to load " << script << std::endl;
     }
 }
 
@@ -62,32 +60,33 @@ int main() {
     config.scriptReader = &LoadScript;
     config.logHandler = &Log;
     OCG_Duel duel;
-    std::error_code error;
-    std::vector<std::filesystem::directory_entry> cardScripts;
-    auto listing = std::filesystem::directory_iterator(".");
     if (OCG_CreateDuel(&duel, config) != OCG_DUEL_CREATION_SUCCESS) {
         std::cerr << "Failed to create duel instance!" << std::endl;
+        return EXIT_FAILURE;
     }
     LoadIfExists(duel, "constant.lua");
     LoadIfExists(duel, "utility.lua");
-    std::copy_if(begin(listing), end(listing), std::back_inserter(cardScripts),
-                 [](const std::filesystem::directory_entry &file) {
-                     return file.is_regular_file() &&
-                            file.path().extension() == ".lua" &&
-                            file.path().filename().string().length() > 1 &&
-                            file.path().filename().string() != "constant.lua" &&
-                            file.path().filename().string()[0] == 'c';
-                 });
-    std::for_each(cardScripts.begin(), cardScripts.end(), [duel](const std::filesystem::directory_entry &file) {
-        std::string name = file.path().filename().string();
-        OCG_NewCardInfo card;
-        card.team = card.duelist = card.con = 0;
-        card.seq = 1;
-        card.loc = LOCATION_DECK;
-        card.pos = POS_FACEDOWN_ATTACK;
-        card.code = std::stoi(name.substr(1, name.length()-4));
-        OCG_DuelNewCard(duel, card);
-    });
+    auto listing = opendir(".");
+    if (!listing) {
+        std::cerr << "Failed to open current directory!" << std::endl;
+        return EXIT_FAILURE;
+    }
+    for (dirent *entry; (entry = readdir(listing)) != nullptr; ) {
+        if (entry->d_type == DT_REG) {
+            std::string name(entry->d_name);
+            auto length = name.length();
+            if (length > 0 && name != "constant.lua" && name != "utility.lua" &&
+                name[0] == 'c' && name.rfind(".lua") == length - 4) {
+                    OCG_NewCardInfo card;
+                    card.team = card.duelist = card.con = 0;
+                    card.seq = 1;
+                    card.loc = LOCATION_DECK;
+                    card.pos = POS_FACEDOWN_ATTACK;
+                    card.code = std::stoi(name.substr(1, length - 4));
+                    OCG_DuelNewCard(duel, card);
+            }
+        }
+    }
     OCG_DestroyDuel(duel);
     return exitCode;
 }
